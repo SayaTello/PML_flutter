da724ca4fdc272afe74325d128cf2ceb6184f6e5
TELLO <201240001068@unisnu.ac.id>
2024-01-11 23:39:38 +0700

    Flutter Integration API Login, Logout, Auth Local, Deployment Laravel App to Server

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,4 @@
 class GlobalVariables {
-  static const baseUrl = 'https://855d-182-253-126-20.ngrok.io';
+  // static const baseUrl = 'https://855d-182-253-126-20.ngrok.io';
+  static const baseUrl = 'https://flutterdev.my.id';
 }
--- lib/data/datasources/auth_remote_datasource.dart
+++ lib/data/datasources/auth_remote_datasource.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 import 'package:tugas_flutter/common/global_variables.dart';
+import 'package:tugas_flutter/data/datasources/auth_local_datasource.dart';
 import 'package:tugas_flutter/data/models/auth_response_model.dart';
+import 'package:tugas_flutter/data/models/request/login_request_model.dart';
 import 'package:tugas_flutter/data/models/request/register_request_model.dart';
 
 class AuthRemoteDatasource {
@@ -18,6 +22,44 @@ class AuthRemoteDatasource {
 
     if (response.statusCode == 200) {
       return Right(AuthResponseModel.fromJson(response.body));
+    } else {
+      return const Left('Server error');
+    }
+  }
+  
+  Future<Either<String, AuthResponseModel>> login(
+      LoginRequestModel model) async {
+    final headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    };
+    final response = await http.post(
+        Uri.parse('${GlobalVariables.baseUrl}/api/login'),
+        headers: headers,
+        body: model.toJson());
+
+    if (response.statusCode == 200) {
+      return Right(AuthResponseModel.fromJson(response.body));
+    } else {
+      final obj = jsonDecode(response.body);
+      return Left(obj['message']);
+    }
+  }
+
+  Future<Either<String, String>> logout() async {
+    final token = await AuthLocalDatasource().getToken();
+    final headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+    final response = await http.post(
+      Uri.parse('${GlobalVariables.baseUrl}/api/logout'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      return const Right('logout success');
     } else {
       return const Left('Server error');
     }
